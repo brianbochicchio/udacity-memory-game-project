101f9b59a4cdfff6878cd6efdd935c1d7c64f2fa
Brian Bochicchio <bbochicchio@gmail.com>
2018-11-01 07:21:29 -0500

    Late push, still need to implement win modal

--- README.md
+++ README.md
@@ -1,10 +1,54 @@
-# udacity-memory-game-project
-## Front-End Dev Memory Game Project
+# Memory Game Project
 
-#### Summary:
-Create a memory matching game with 8 pairs of cards.  The game must track and display move count and playtime. The game window must also include a game reset button. This resets the board, timer and move count.
+## Table of Contents
 
-At the end of the game the user should see a modal with playtime, rating and the option to play again. 
+* [Summary](#summary)
+* [Requirments](#requirements)
+
+## Credits 
+This project is based on the starter project provided to Udacity Front-End Developer Nanodegree Students on [GitHub](https://github.com/udacity/fend-project-memory-game)
+
+## Summary
+This is a 16-card memory matching game for block 3,  JavaScript and the DOM. 
+
+## Requirements
+Based on the provided rubric the project meets the following mandatory and optional requirements
+
+* Game board elements:
+    * Game Title 
+    * Deck of 16 Cards
+    * Star Rating
+    * Moves Counter
+    * Game Timer
+    * Game Reset Button
+    
+* Other elements
+    * Modal "Win" Pop-up with the following elements
+        * Congratulations Message
+        * Play again query
+        * Game Time
+        * Star Rating
+         
+* Functionality
+    * Must randomly shuffle cards on game start and reset
+    * Must reset the board, star rating, move count and timer when reset is pressed
+    * Must show "Win" modal when all cards are matched (Not Implemented)
+    * Must countdown start rating from 3 to 1
+    * Must show and increment move counter
+    * Must show game time in MM:SS 
+        * Starts with first card being "flipped"
+        * Stops when player wins or game is reset
+        
+* Code Quality
+    * Code is consistent, logical and follow style guides
+    * Concerns are separated
+        * Function calls take or call an appropriate supporting function 
+            * flipCard shouldn't change the clock state
+            * flipCard can call the function to check for a match when a card is flipped
+
+
+
+
 
 
 
---
+++ js/app.js
@@ -0,0 +1,259 @@
+/*
+ * Game Setup
+ */
+
+let deckOfCards = ["fa-diamond",
+    "fa-paper-plane-o",
+    "fa-anchor",
+    "fa-bolt",
+    "fa-cube",
+    "fa-anchor",
+    "fa-leaf",
+    "fa-bicycle",
+    "fa-diamond",
+    "fa-bomb",
+    "fa-leaf",
+    "fa-bomb",
+    "fa-bolt",
+    "fa-bicycle",
+    "fa-paper-plane-o",
+    "fa-cube"]
+
+// Constants to minimize magic numbers and make changing the game attributes easier
+const winningCardCount = 16;
+const cardHideDelay = 1000;
+const numberOfStars = 3;
+
+// Document references that are used frequently or needed for game duration
+const moveCounter = document.querySelector('.moves');
+const gameRestart = document.querySelector('.restart');
+const deckEventHandler = document.querySelector('.deck');
+
+
+//Globals for frequently referenced or updated variables
+let moveCount;
+let elapsedTime;
+let gameClock;
+let flippedCardCount;
+
+// Container for selected cards
+let selectedCards = [];
+
+// True when the when the cards don't match and until they are flipped back over to prevent errant flips
+let interceptClick = false;
+
+// Game Board event listeners
+gameRestart.addEventListener('click', resetBoard);
+
+//Handles card clicks and initiates end game check
+deckEventHandler.addEventListener('click', function (e) {
+    if (e.target.className === "card" && !interceptClick) {
+        flipCard(e.target)
+        //Allows last card to draw before checking for win condition
+        window.setTimeout(gameWon, 250);
+    }
+});
+
+
+/*
+ * Game State Functions
+ */
+
+function gameInit() {
+    moveCount = 0;
+    elapsedTime = 0;
+    flippedCardCount = 0;
+    moveCounter.innerText = moveCount;
+    shuffle(deckOfCards);
+    drawBoard(deckOfCards, numberOfStars);
+};
+
+function gameWon() {
+
+    if (flippedCardCount == winningCardCount) {
+
+        console.log("GAME WON");
+        clockStop();
+    }
+
+};
+
+/*
+ * Start the Game *
+ */
+
+gameInit();
+
+
+/*
+ * Game Board Functions *
+ */
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+function drawBoard(cards, stars) {
+
+    let deckLocation = document.querySelector('.deck');
+
+    for (card of cards) {
+        deckLocation.insertAdjacentHTML("beforeend", "<li class=\"card\"><i class=\"fa " + card + "\"></i></li>");
+    }
+
+    let starsLocation = document.querySelector(".stars");
+    for (let i = 0; i < stars; i++ ) {
+        starsLocation.insertAdjacentHTML("beforeend", "<li><i class=\"fa fa-star\"></i></li>");
+    }
+
+}
+
+function resetBoard() {
+
+    selectedCards = [];
+
+    let deckContainer = document.querySelector('.deck');
+    while (deckContainer.firstChild) {
+        deckContainer.removeChild(deckContainer.firstChild);
+    }
+
+    let starsLocation = document.querySelector(".stars");
+    while (starsLocation.firstChild) {
+        starsLocation.removeChild(starsLocation.firstChild);
+    }
+
+    clockStop();
+    let timerLocation = document.querySelector('.timer');
+    timerLocation.innerText = "-- : --";
+
+    gameInit();
+
+}
+
+function updateMoves(){
+    moveCount++;
+    moveCounter.innerText = moveCount;
+
+
+    switch (moveCount) {
+        case 14:
+            removeStar();
+            break;
+        case 20:
+            removeStar();
+            break;
+        default:
+
+    }
+
+}
+
+
+function removeStar() {
+
+    let starsLocation = document.querySelector(".stars").firstElementChild;
+    starsLocation.parentNode.removeChild(starsLocation);
+    console.log("remove stars called");
+}
+
+/*
+ * Clock Functions *
+ */
+
+function clockStart(){
+    gameClock = setInterval(clockTick, 1000);
+
+}
+
+function clockStop(){
+    clearInterval(gameClock);
+
+}
+
+function clockTick() {
+    let timerLocation = document.querySelector('.timer');
+    let minute = Math.floor(elapsedTime / 60);
+    let seconds = Math.floor(elapsedTime - (minute * 60))  ;
+    minute = minute.toString();
+    seconds = seconds.toString();
+    elapsedTime++;
+    timerLocation.innerText = minute.padStart(2,'0')+ " : " + seconds.padStart(2,'0');
+
+}
+
+
+/*
+ * Game Card Functions *
+ */
+
+
+function flipCard(card) {
+    if (elapsedTime === 0){
+        clockStart();
+    }
+
+    if (selectedCards.length < 2) {
+        showCard(card);
+    }
+
+    if (selectedCards.length === 2) {
+        isMatch(selectedCards);
+        updateMoves();
+    }
+
+
+}
+
+function showCard(card) {
+    selectedCards.push(card);
+    card.classList.add('open', 'show');
+}
+
+
+function isMatch(cards) {
+    let card1 = cards[0].childNodes[0].classList[1];
+    let card2 = cards[1].childNodes[0].classList[1];
+
+
+    if (card1 == card2) {
+        showMatch(selectedCards);
+        selectedCards = [];
+        flippedCardCount += 2;
+    } else {
+        interceptClick = true;
+        window.setTimeout(hideCards, cardHideDelay, selectedCards);
+
+    }
+
+}
+
+function showMatch(cards) {
+    for (card of cards) {
+        card.classList.remove('open', 'show');
+        card.classList.add('match');
+    }
+
+}
+
+function hideCards(cards) {
+    for (card of cards) {
+        card.classList.remove('open', 'show');
+    }
+    selectedCards = [];
+    interceptClick = false;
+
+}
+
+
+
